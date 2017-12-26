3968a404309c0bda60cdb4ffa7fc4f6165b85f1d
Wang <jake.jian.wang@accenture.com>
2017-12-26 17:32:10 +0800

    commit

--- src/update1day.js
+++ src/update1day.js
@@ -3,7 +3,7 @@ function(){
 
 var updateonedayvacation ={}
 
-  updateonedayvacation.updateVacByDay = function(id,data_param){
+  updateonedayvacation.updateVacByDay = function(data_param){
 
  var result =0;
 
@@ -28,36 +28,38 @@ data_param = [{"name":"jake.jian.wang","team":"mf11","workdate":20171219,"type":
             crossDomain: true,
             success: function (data) {
                 alert("JSON Data ID: " + data.name);
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                result = -1
-                alert("status=" + XMLHttpRequest.status);
-                alert("readyState=" + XMLHttpRequest.readyState);
-                alert("textStatus=" + textStatus);
-            },
-            complete: function (XMLHttpRequest, textStatus) {
-              result = 1
-            }
-        });
+           
 
-//AJAX操作step2： 把JSON对象Array一次插入到DB
-        $.ajax({
-            //url: "http://10.1.251.111:8080/ora/db/query",
-            url: "http://localhost:8080/OraDAO-0.0.1-SNAPSHOT/workschedule/adds",
-            type: "POST",
-            data:JSON.stringify(data_param),
-            headers: {
-                "Content-Type": "application/json"
-            },
-            contentType: 'application/json"; charset=UTF-8',
-            dataType: "json",
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain: true,
-            success: function (data) {
-                alert("JSON Data ID: " + data.name);
-            },
+                //AJAX操作step2： 把JSON对象插入到DB
+                    $.ajax({
+                        //url: "http://10.1.251.111:8080/ora/db/query",
+                        url: "http://localhost:8080/OraDAO-0.0.1-SNAPSHOT/workschedule/adds",
+                        type: "POST",
+                        data:JSON.stringify(data_param),
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        contentType: 'application/json"; charset=UTF-8',
+                        dataType: "json",
+                        xhrFields: {
+                            withCredentials: true
+                        },
+                        crossDomain: true,
+                        success: function (data) {
+                            alert("JSON Data ID: " + data.name);
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            result = -1
+                            alert("status=" + XMLHttpRequest.status);
+                            alert("readyState=" + XMLHttpRequest.readyState);
+                            alert("textStatus=" + textStatus);
+                        },
+                        complete: function (XMLHttpRequest, textStatus) {
+                            result = 1
+                        }
+                    });
+
+         },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 result = -1
                 alert("status=" + XMLHttpRequest.status);
